18dd89046f39a4f13fb999c3b7c6695f03409f15
L00P3R93 <vincentkioko51@gmail.com>
2024-10-08 12:29:59 +0300

    Auth & Session Mgt

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,16 @@
 import { useRouter } from 'expo-router';
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-import ScreenWrapper from '../components/ScreenWrapper';
+
+import Loading from '../components/Loading';
 
 const index = () => {
     const router = useRouter();
 
     return (
-        <ScreenWrapper>
-            <Text>Index</Text>
-            <Button title="Welcome" onPress={() => router.push('welcome')} />
-        </ScreenWrapper>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Loading />
+        </View>
     );
 };
 
